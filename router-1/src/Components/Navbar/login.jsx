a06c85146241742e015be20ff9543e86cd4accf0
ShashankSKB <shashankborkar16@gmail.com>
2021-11-21 21:49:03 +0530

    assign

---
+++ router-1/src/Components/Navbar/login.jsx
@@ -0,0 +1,12 @@
+function Login()
+{
+
+
+    return (
+        <div>
+            <h1>About page</h1>
+        </div>
+    )
+}
+
+export {Login}
---
+++ router-1/src/Components/Navbar/servicepage.jsx
@@ -0,0 +1,12 @@
+function Services()
+{
+
+
+    return (
+        <div>
+            <h1>Services page</h1>
+        </div>
+    )
+}
+
+export {Services}
